07af1dd5e19dc59cff78a7561a5adb838482e273
irvoma <irvoma@gmail.com>
2024-08-18 09:23:05 -0600

    refactor: change list app urls to better reading

--- lists/urls.py
+++ lists/urls.py
@@ -5,8 +5,7 @@ from lists import views
 app_name = 'lists'
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('lists/new', views.new_list, name='new_list'),
-    path('lists/<int:list_id>/', views.view_list, name='view_list'),
-    path('lists/<int:list_id>/add_item', views.add_item, name='add_item'),
+    path('new', views.new_list, name='new_list'),
+    path('<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/add_item', views.add_item, name='add_item'),
 ]
