aad23482ed809e5669482265c0a287a6d27b864d
irvoma <irvoma@gmail.com>
2024-09-10 13:50:17 -0600

    feat: use save from form to save items

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.core.validators import ValidationError
-from lists.models import Item, List
+from lists.models import List
 from lists.forms import ItemForm
 
 # Create your views here.
@@ -14,7 +13,7 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'lists/list.html', {'list': list_, 'form': form})
 
@@ -23,7 +22,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'lists/home.html', {'form': form})
