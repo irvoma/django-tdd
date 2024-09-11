29c6a5493953ecebc81f725689f74aedf58fef2c
irvoma <irvoma@gmail.com>
2024-09-11 11:42:43 -0600

    feat: add uniqueness validation to form

--- lists/forms.py
+++ lists/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from lists.models import Item
 
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.models.ModelForm):
     def save(self, for_list):
@@ -23,3 +26,22 @@ class ItemForm(forms.models.ModelForm):
                 'required': EMPTY_ITEM_ERROR,
             }
         }
+
+
+class ExistingListItemForm(ItemForm):
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_constraints(self):
+        """ The book suggests to use validate_unique function, but unique together is
+            defined using constraints Meta class option, and the defined constraints are
+            validated through the validate_constraints function
+            Further info at https://docs.djangoproject.com/en/4.2/ref/models/instances/#django.db.models.Model.validate_unique
+        """
+        print('validating constraints')
+        try:
+            self.instance.validate_constraints()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 from django.test import TestCase
 from django.utils.html import escape
 
@@ -90,6 +92,16 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='item 1')
+        response = self.client.post(f'/lists/{list1.id}/', data={'text': 'item 1'})
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'lists/list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
